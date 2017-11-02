457be62d56a448982847704d7da9cf0708c77f67
denyson <814149403@qq.com>
2017-11-02 18:30:02 +0800

    交作业

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice07ColorMatrixColorFilterView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice07ColorMatrixColorFilterView.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -29,9 +32,17 @@ public class Practice07ColorMatrixColorFilterView extends View {
 
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
-
         // 使用 setColorFilter() 设置一个 ColorMatrixColorFilter
         // 用 ColorMatrixColorFilter.setSaturation() 把饱和度去掉
+
+        ColorMatrix colorMatrix = new ColorMatrix(new float[]{1f, 0.2f, 0.1f, 0.25f, 1f,
+                0.2f, 0.6f, 1f, 0.25f, 0.5f,
+                0.2f, 0.2f, 0f, 0.2f, 1f, 0.4f,
+                1f, 0.55f, 0.12f, 0.2f, 0.2f});
+
+        colorMatrix.setSaturation(0);
+        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        paint.setColorFilter(colorFilter);
     }
 
     @Override
--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
@@ -3,7 +3,9 @@ package com.hencoder.hencoderpracticedraw2.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
+import android.graphics.MaskFilter;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.support.annotation.Nullable;
@@ -40,15 +42,23 @@ public class Practice14MaskFilterView extends View {
         // 用 Paint.setMaskFilter 来设置不同的 BlurMaskFilter
 
         // 第一个：NORMAL
+        MaskFilter maskFilter = new BlurMaskFilter(30, BlurMaskFilter.Blur.NORMAL);
+        paint.setMaskFilter(maskFilter);
         canvas.drawBitmap(bitmap, 100, 50, paint);
 
         // 第二个：INNER
+        MaskFilter INNER = new BlurMaskFilter(30, BlurMaskFilter.Blur.INNER);
+        paint.setMaskFilter(INNER);
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, 50, paint);
 
         // 第三个：OUTER
+        MaskFilter OUTER = new BlurMaskFilter(30, BlurMaskFilter.Blur.OUTER);
+        paint.setMaskFilter(OUTER);
         canvas.drawBitmap(bitmap, 100, bitmap.getHeight() + 100, paint);
 
         // 第四个：SOLID
+        MaskFilter SOLID = new BlurMaskFilter(30, BlurMaskFilter.Blur.SOLID);
+        paint.setMaskFilter(SOLID);
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, bitmap.getHeight() + 100, paint);
     }
 }
